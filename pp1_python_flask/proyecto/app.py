53f490472fd01e263f52b679164295c6898a9cf3
FranAmbrogio7 <franciscoambrogio@gmail.com>
2025-06-27 16:37:43 -0300

    update

--- pp1_python_flask/proyecto/app.py
+++ pp1_python_flask/proyecto/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index ():
-    return 'Hola Mundo!'
+    return 'Hello World! Welcome to my Flask app'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) 
