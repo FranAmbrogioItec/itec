75fe1b2a469efbd6ed067a6317126c43f9ee8757
FranAmbrogioItec <f.ambrogio@itecriocuarto.org.ar>
2026-06-21 18:53:54 -0300

    feat: integracion de crud completo para el modelo Plato

--- 3ero/ing_software/tp_gastronomia/core/urls.py
+++ 3ero/ing_software/tp_gastronomia/core/urls.py
@@ -1,9 +1,21 @@
 # core/urls.py
 from django.urls import path
-from .views import IndexView
+from .views import (
+    IndexView, 
+    PlatoListView, 
+    PlatoCreateView, 
+    PlatoUpdateView, 
+    PlatoDeleteView
+)
 
 app_name = 'core'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    
+    # crud platos
+    path('platos/', PlatoListView.as_view(), name='plato_list'),
+    path('platos/nuevo/', PlatoCreateView.as_view(), name='plato_create'),
+    path('platos/<int:pk>/editar/', PlatoUpdateView.as_view(), name='plato_update'),
+    path('platos/<int:pk>/eliminar/', PlatoDeleteView.as_view(), name='plato_delete'),
 ]
--- 3ero/ing_software/tp_gastronomia/core/views.py
+++ 3ero/ing_software/tp_gastronomia/core/views.py
@@ -1,5 +1,31 @@
 # core/views.py
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Plato
 
 class IndexView(TemplateView):
     template_name = 'core/index.html'
+
+# --- CRUD DE PLATOS ---
+
+class PlatoListView(ListView):
+    model = Plato
+    template_name = 'core/plato_list.html'
+    context_object_name = 'platos'
+
+class PlatoCreateView(CreateView):
+    model = Plato
+    template_name = 'core/plato_form.html'
+    fields = ['nombre', 'descripcion', 'precio', 'categoria', 'imagen']
+    success_url = reverse_lazy('core:plato_list')
+    
+class PlatoUpdateView(UpdateView):
+    model = Plato
+    template_name = 'core/plato_form.html'
+    fields = ['nombre', 'descripcion', 'precio', 'categoria', 'imagen']
+    success_url = reverse_lazy('core:plato_list')
+
+class PlatoDeleteView(DeleteView):
+    model = Plato
+    template_name = 'core/plato_confirm_delete.html'
+    success_url = reverse_lazy('core:plato_list')
